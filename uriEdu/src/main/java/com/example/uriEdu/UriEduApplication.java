fbc29430880f61a4174ef222d64471c1886d8bb1
quokkaDH <123261373+quokkaDH@users.noreply.github.com>
2024-02-12 16:18:47 +0900

    Github test

---
+++ uriEdu/src/main/java/com/example/uriEdu/UriEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.uriEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UriEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UriEduApplication.class, args);
+	}
+
+}
