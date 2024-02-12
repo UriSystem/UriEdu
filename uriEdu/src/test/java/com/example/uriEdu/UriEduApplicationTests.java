9d246aef2a5075eb575b1f0678a5a3511dc1f44d
quokkaDH <123261373+quokkaDH@users.noreply.github.com>
2024-02-12 16:27:37 +0900

    Revert "Revert "Github test""
    This reverts commit f8e7aa61c92614f0b6cc772e0de57e6f72b23c10.

---
+++ uriEdu/src/test/java/com/example/uriEdu/UriEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uriEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UriEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
